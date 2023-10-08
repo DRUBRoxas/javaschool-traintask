6aaa2d08fdb7e755365a1cb30125c7bef908c94f
Manuel Sanchez Salazar <mss00048@red.ujaen.es>
2023-10-08 14:37:02 +0200

    Merge

---
+++ src/test/java/com/javaschool/finaltask/FinaltaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaschool.finaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
